34bd7b29867d305af7db14005e7f57ae0b8993f6
Yuxiang Ma <n10129375@qut.edu.au>
2020-10-19 01:11:08 +1000

    12345

--- assign2/routes/twitter.js
+++ assign2/routes/twitter.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const Twitter = require('twitter');
+var analyzer = require('natural').SentimentAnalyzer;
+var stemmer = require('natural').PorterStemmer;
 const router = express.Router();
 require('dotenv').config();
 
-const server = 'http://localhost:3000';
+
+
+var sentimentAnalyzer = new analyzer("English", stemmer, "afinn");
 
 const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -12,18 +16,73 @@ const client = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-router.get('/get/:q', function (req, res, next) {
-    client.get('search/tweets', { q: req.params.q }, function (error, tweets, response) {
+
+// client.get('trends/place', {id: '23424748' }, function (error, trend){
+//     let trends = trend[0].trends;
+//     console.log(trends);
+// });
+
+router.get('/trends', function (req, res, next) {
+    client.get('trends/place', {id: '23424748'}, function (error, trends, response) {
         if (!error) {
+            console.log(trends[0].trends);
             res.render('twitter', {
-                title: 'Latest Tweets',
+                title: 'trending',
                 keyword: req.params.q,
-                data: tweets.statuses
+                data: trends[0].trends
             });
             res.end();
         } else {
             console.error(error);
         }
+    });
+});
+
+async  function processTweets(tweetsBody) {
+  try{
+    return new Promise((resolve) => {
+      var TweetFullText = '';
+      if(tweetsBody != undefined){
+        if(tweetsBody.extended_tweet){
+          TweetFullText = tweetsBody.extended_tweet.full_text;
+        } else {
+          TweetFullText = tweetsBody.text;
+        }
+      } 
+      resolve(TweetFullText);
+    });
+  }catch(error){
+    console.log(error);
+  }
+}
+
+  
+  async function Sentiment(tweet) {
+    try{
+      return new Promise((resolve) => {
+        var splitTweet = tweet.split(' ');
+        let sentiment = sentimentAnalyzer.getSentiment(splitTweet);
+        resolve(sentiment);
+      });
+    } catch(error){
+      console.log(error);
+    }
+  };
+
+  
+router.get('/stream', function (req, res, next){
+    console.log(req.query.q);
+    client.stream('statuses/filter', {track: req.query.q , language: 'en' }, function (stream) {
+        stream.on('data', function(event) {
+          processTweets(event).then(result =>{
+            Sentiment(result).then(results => {
+              console.log(results);
+            })
+          })
+        });
+        stream.on('error', function(error) {
+          throw error;
+        });
     });
 });
 
