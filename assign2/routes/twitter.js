647f8fafa1ee9f57b810963ad968f6125dcf00a2
Zheng He <n10127135@qut.edu.au>
2020-10-20 00:24:27 +0800

    Update twitter.js
    ·

--- assign2/routes/twitter.js
+++ assign2/routes/twitter.js
@@ -7,83 +7,73 @@ require('dotenv').config();
 
 
 
-var sentimentAnalyzer = new analyzer("English", stemmer, "afinn");
+const sentimentAnalyzer = new analyzer("English", stemmer, "afinn");
 
 const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
-
-
-// client.get('trends/place', {id: '23424748' }, function (error, trend){
-//     let trends = trend[0].trends;
-//     console.log(trends);
-// });
 
 router.get('/trends', function (req, res, next) {
-    client.get('trends/place', {id: '23424748'}, function (error, trends, response) {
-        if (!error) {
-            console.log(trends[0].trends);
-            res.render('twitter', {
-                title: 'trending',
-                keyword: req.params.q,
-                data: trends[0].trends
-            });
-            res.end();
-        } else {
-            console.error(error);
-        }
-    });
+  client.get('trends/place', { id: '23424748' }, function (error, trends, response) {
+    if (!error) {
+      res.render('twitter', {
+        title: 'trending',
+        keyword: req.params.q,
+        data: trends[0].trends
+      });
+      res.end();
+    } else {
+      console.error(error);
+    }
+  });
 });
 
-async  function processTweets(tweetsBody) {
-  try{
+async function Sentiment(tweet) {
+  try {
     return new Promise((resolve) => {
-      var TweetFullText = '';
-      if(tweetsBody != undefined){
-        if(tweetsBody.extended_tweet){
-          TweetFullText = tweetsBody.extended_tweet.full_text;
-        } else {
-          TweetFullText = tweetsBody.text;
-        }
-      } 
-      resolve(TweetFullText);
+      var splitTweet = tweet.split(' ');
+      let sentiment = sentimentAnalyzer.getSentiment(splitTweet);
+      resolve(sentiment);
     });
-  }catch(error){
-    console.log(error);
+  } catch (error) {
+    console.error(error);
   }
-}
+};
 
-  
-  async function Sentiment(tweet) {
-    try{
-      return new Promise((resolve) => {
-        var splitTweet = tweet.split(' ');
-        let sentiment = sentimentAnalyzer.getSentiment(splitTweet);
-        resolve(sentiment);
-      });
-    } catch(error){
-      console.log(error);
+let stream = undefined;
+
+router.get('/stream', function (req, res, next) {
+  // check if exist there is a stream before, delete it.
+  if (stream) {
+    stream.destroy();
+    console.log('Old stream destroyed.');
+  }
+
+  console.log('New stream created with query: ' + req.query.q);
+
+  stream = client.stream('statuses/filter', { track: req.query.q, language: 'en' });
+
+  stream.on('data', function (event) {
+    if (event && !event.retweeted_status) {
+
+      // process tweet
+      let text = event.extended_tweet ? event.extended_tweet.full_text : event.text;
+      console.log(event.user.screen_name + " : " + text);
+      
+      // get sentiment rate from tweet
+      Sentiment(text).then(results => {
+        console.log(results);
+        console.log('---------------------------------------');
+      })
     }
-  };
+  });
 
-  
-router.get('/stream', function (req, res, next){
-    console.log(req.query.q);
-    client.stream('statuses/filter', {track: req.query.q , language: 'en' }, function (stream) {
-        stream.on('data', function(event) {
-          processTweets(event).then(result =>{
-            Sentiment(result).then(results => {
-              console.log(results);
-            })
-          })
-        });
-        stream.on('error', function(error) {
-          throw error;
-        });
-    });
+  stream.on('error', function (error) {
+    console.error(error);
+  });
 });
 
 module.exports = router;
